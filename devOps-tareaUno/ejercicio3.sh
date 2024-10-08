473e8313e50b9c6900ee5777a9668553ba308d37
manuel monjes <manuelmonjes@gmail.com>
2024-10-08 09:22:57 -0300

    aritmetica basica

---
+++ devOps-tareaUno/ejercicio3.sh
@@ -0,0 +1,14 @@
+echo "ingresa el primer numero"
+read num1
+echo "ingresa el segundo numero"
+read num2
+
+suma=$((num1 + num2))
+resta=$((num1 - num2))
+multiplicacion=$((num1 * num2))
+division=$((num1 / num2))
+
+echo "Suma: $suma"
+echo "Resta: $resta"
+echo "Multiplicacion: $multiplicacion"
+echo "Division: $division"
